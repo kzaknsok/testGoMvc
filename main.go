b578292f0a3901ca69719dd706b2740d31f3509c
kzaknsok <eightymurasame@gmail.com>
2023-08-30 11:29:34 +0900

    second time

--- main.go
+++ main.go
@@ -1,1 +1,8 @@
 package main
+
+import "example.com/server"
+
+func main() {
+	r := server.GetRouter()
+	r.Run(":8080")
+}
--- server/router.go
+++ server/router.go
@@ -1,12 +1,19 @@
 package server
 
 import (
+	"example.com/controller/top"
+	"example.com/controller/user"
 	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("view/*.html")
+
+	r.GET("/", top.IndexDisplayAction)
+	r.GET("/user", user.UserListDisplayAction)
+	r.GET("/user/add", user.UserAddDisplayAction)
+	r.GET("/user/edit/:id", user.UserEditDisplayAction)
 
 	return r
 }
